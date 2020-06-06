06f922e8cbbc4ecbd08d1c6b8546d54ca075c8f1
phatdroid94 <phatdroid94@gmail.com>
2020-06-06 20:33:28 +0700

    flow

--- app/src/main/java/com/example/fragmentnavigation25022020/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/fragmentnavigation25022020/ForgotPasswordFragment.java
@@ -3,16 +3,45 @@ package com.example.fragmentnavigation25022020;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 public class ForgotPasswordFragment extends Fragment {
 
+    View mView;
+    EditText mEdtEmail;
+    Button mBtnReset,mBtnExit;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false);
+        mView = inflater.inflate(R.layout.fragment_forgot_password, container, false);
+        mEdtEmail = mView.findViewById(R.id.edtEmail);
+        mBtnExit = mView.findViewById(R.id.btnExitForgot);
+        mBtnReset = mView.findViewById(R.id.btnReset);
+
+        mBtnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment
+                        .findNavController(ForgotPasswordFragment.this)
+                        .popBackStack();
+            }
+        });
+
+        mBtnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment
+                        .findNavController(ForgotPasswordFragment.this)
+                        .popBackStack();
+            }
+        });
+        return mView;
     }
 }
--- app/src/main/java/com/example/fragmentnavigation25022020/LogoutFragment.java
+++ app/src/main/java/com/example/fragmentnavigation25022020/LogoutFragment.java
@@ -3,16 +3,34 @@ package com.example.fragmentnavigation25022020;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 public class LogoutFragment extends Fragment {
 
+    View mView;
+    Button mBtnLogout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logout, container, false);
+
+        mView = inflater.inflate(R.layout.fragment_logout, container, false);
+        mBtnLogout = mView.findViewById(R.id.buttonLogout);
+
+        mBtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment
+                        .findNavController(LogoutFragment.this)
+                        .popBackStack();
+            }
+        });
+
+        return mView;
     }
 }
